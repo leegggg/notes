dd8013012b4c091d1a27828dfc86e23257647e5f
LIN Yizhou <yizhou.lin@outlook.com>
2021-11-02 10:03:16 +0800

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,1 +1,7 @@
 find . -type f -name '*.tar*' -exec sh -c 'set -x && fp=$(basename "${0}") && echo ${fp}&& cd $( dirname "${0}") && pwd && tar xvf "${fp}"|| /home/admin/pytar.py "${fp}" && rm "${fp}"' {} ";"
+mkdir -p /home/admin/nfs/ext_hdd/by-uuid/792f935f-9bdb-46b6-a7c4-df74519c9b08/pron/
+find . -maxdepth 3 -mindepth 3 -type d -exec sh -c 'set -x && mv "${0}" ../pron/' {} ";"
+mkdir -p /home/admin/nfs/ext_hdd/by-uuid/792f935f-9bdb-46b6-a7c4-df74519c9b08/pron2/
+find . -maxdepth 3 -mindepth 3 -type d -exec sh -c 'set -x && mv "${0}" ../pron2/' {} ";"
+mkdir -p /home/admin/nfs/ext_hdd/by-uuid/792f935f-9bdb-46b6-a7c4-df74519c9b08/pron-file/
+find . -maxdepth 3 -mindepth 3 -type d -exec sh -c 'set -x && mv "${0}" ../pron-file/' {} ";"
